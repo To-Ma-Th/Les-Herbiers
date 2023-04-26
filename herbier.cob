08e7c7c41a204b97b27543fe3f67a03e58e66989
RedNeath <theandre.bourry@gmail.com>
2023-04-26 20:22:39 +0200

    [FEAT] Connexion utilisateur (+ boucle programme principal)

--- herbier.cob
+++ herbier.cob
@@ -4,78 +4,78 @@ PROGRAM-ID. Herbier.
 ENVIRONMENT DIVISION.
 INPUT-OUTPUT SECTION.
 FILE-CONTROL. 
-        select fplan assign to "plante.dat"
-        organization indexed
-        access mode is dynamic
-        record key is pl_id
-        alternate record key is pl_nom
-        alternate record key is pl_nomLatin
-        alternate record key is pl_saison WITH DUPLICATES
-        file status is cr_fplan.
-        
-        select fher assign to "herbier.dat"
-        organization indexed
-        access mode is dynamic
-        record key is fh_id
-        alternate record key is fh_utilisateur WITH DUPLICATES
-        alternate record key is fh_type WITH DUPLICATES
-        file status is cr_fher.
-        
-        select futil assign to "utilsateur.dat"
-        organization indexed
-        access mode is dynamic
-        record key is fu_id
-        alternate record key is fu_login
-        alternate record key is fu_role WITH DUPLICATES
-        alternate record key is fu_type WITH DUPLICATES
-        file status is cr_futil.
-        
-        select fhpl assign to "herbier_plante.dat"
-        organization indexed
-        access mode is dynamic
-        record key is fhpl_id
-        alternate record key is fhpl_lieu WITH DUPLICATES
-        alternate record key is fhpl_idHerbier WITH DUPLICATES
-        alternate record key is fhpl_idPlante WITH DUPLICATES
-        file status is cr_fhpl.
-        
-        
+       select fplan assign to "plante.dat"
+       organization indexed
+       access mode is dynamic
+       record key is pl_id
+       alternate record key is pl_nom
+       alternate record key is pl_nomLatin
+       alternate record key is pl_saison WITH DUPLICATES
+       file status is cr_fplan.
+
+       select fher assign to "herbier.dat"
+       organization indexed
+       access mode is dynamic
+       record key is fh_id
+       alternate record key is fh_utilisateur WITH DUPLICATES
+       alternate record key is fh_type WITH DUPLICATES
+       file status is cr_fher.
+
+       select futil assign to "utilsateur.dat"
+       organization indexed
+       access mode is dynamic
+       record key is fu_id
+       alternate record key is fu_login
+       alternate record key is fu_role WITH DUPLICATES
+       alternate record key is fu_type WITH DUPLICATES
+       file status is cr_futil.
+
+       select fhpl assign to "herbier_plante.dat"
+       organization indexed
+       access mode is dynamic
+       record key is fhpl_id
+       alternate record key is fhpl_lieu WITH DUPLICATES
+       alternate record key is fhpl_idHerbier WITH DUPLICATES
+       alternate record key is fhpl_idPlante WITH DUPLICATES
+       file status is cr_fhpl.
+
+
 DATA DIVISION.
 FILE SECTION.
 FD fplan.
-        01 tamp_fplan.
-                02 pl_id PIC 9(3).
-                02 pl_nom PIC A(30).
-                02 pl_nomLatin PIC A(45).
-                02 pl_habitat PIC A(15).
-                02 pl_saison PIC A(15).
-                02 pl_duree PIC 9(2).
-                02 pl_type PIC A(15).
+       01 tamp_fplan.
+           02 pl_id PIC 9(3).
+           02 pl_nom PIC A(30).
+           02 pl_nomLatin PIC A(45).
+           02 pl_habitat PIC A(15).
+           02 pl_saison PIC A(15).
+           02 pl_duree PIC 9(2).
+           02 pl_type PIC A(15).
                 
 FD fher.
-        01 tamp_fher.
-                02 fh_id PIC 9(3).
-                02 fh_nom PIC A(30).
-                02 fh_utilisateur PIC 9(3).
-                02 fh_date PIC A(15).
-                02 fh_type PIC A(20).
+       01 tamp_fher.
+           02 fh_id PIC 9(3).
+           02 fh_nom PIC A(30).
+           02 fh_utilisateur PIC 9(3).
+           02 fh_date PIC A(15).
+           02 fh_type PIC A(20).
               
 FD futil.
-        01 tamp_futil.
-                02 fu_id PIC 9(3).
-                02 fu_login PIC A(20).
-                02 fu_mdp PIC A(20).
-                02 fu_role PIC A(15).
-                02 fu_type PIC A(15).
+       01 tamp_futil.
+           02 fu_id PIC 9(3).
+           02 fu_login PIC A(20).
+           02 fu_mdp PIC A(20).
+           02 fu_role PIC A(15).
+           02 fu_type PIC A(15).
                 
 FD fhpl.
-        01 tamp_fhpl.
-                02 fhpl_id PIC 9(2).
-                02 fhpl_idHerbier PIC 9(3).
-                02 fhpl_idPlante PIC 9(3).
-                02 fhpl_date PIC A(15).
-                02 fhpl_taille PIC 9(5).
-                02 fhpl_lieu PIC A(40).
+       01 tamp_fhpl.
+           02 fhpl_id PIC 9(2).
+           02 fhpl_idHerbier PIC 9(3).
+           02 fhpl_idPlante PIC 9(3).
+           02 fhpl_date PIC A(15).
+           02 fhpl_taille PIC 9(5).
+           02 fhpl_lieu PIC A(40).
                
 
                
@@ -101,10 +101,15 @@ WORKING-STORAGE SECTION.
        77 wEndOfFile PIC 9(1).
        77 wUtilisateursCount PIC 9(3).
        77 wConnectedUser PIC 9(3).
+       77 wExitProgramme PIC 9(1).
+       77 wLoginTrialsCount PIC 9(1).
+       77 wPassword PIC A(20).
         
         
 PROCEDURE DIVISION.
 
+*> Initialisation du programme (opérations à faire avant les interac-
+*> tions avec l'utilisateur).
 OPEN I-O fplan
 IF cr_fplan=35 THEN
         OPEN OUTPUT fplan
@@ -141,6 +146,14 @@ MOVE "Mixte" TO CONST_HERBIER_MIXTE.
 
 *> Insertion automatique d'un untilisateur s'il n'y en a pas déjà
 PERFORM add_default_user_if_first_start.
+
+*> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*> La boucle du main (c'est ici qu'il faudra rajouter les instructions à
+*> effectuer pendant la vie du programme, comme l'affichage du menu
+*> principal, ...)
+PERFORM WITH TEST AFTER UNTIL wExitProgramme = 1
+      *> TODO
+END-PERFORM.
 
 STOP RUN.
 
@@ -235,3 +248,59 @@ add_default_user_if_first_start.
            CLOSE futil
        END-IF.
 
+login.
+*> Permet aux utilisateurs de s'authentifier en entrant un nom d'utili-
+*> sateur et un mot de passe. ⚠️ Si la combinaison est incorrecte au
+*> bout de trois essais, le programme doit se fermer.
+*>
+*> Variables utilisées :
+*> - wConnectedUser
+*> - wExitProgramme
+*> - wLoginTrialsCount
+*> - wPassword
+*>
+*> Nombre de lectures :
+*> 1 maximum à chaque demande (trois au pire) puisqu'il s'agit d'une
+*> recherche directe (CSSD)
+       MOVE 0 TO wExitProgramme
+       MOVE 0 TO wLoginTrialsCount
+       DISPLAY "Veuillez entrer votre login, puis votre mot de passe ",
+           "après un retour à la ligne. Vous avez trois tentatives ",
+           "avant que le logiciel ne se ferme."
+
+       PERFORM WITH TEST AFTER UNTIL wLoginTrialsCount = 3
+                              OR NOT wConnectedUser = 0
+          *> On déconnecte l'utilisateur (à voir si on garde cette
+          *> étape)
+           MOVE 0 TO wConnectedUser
+           ADD 1 TO wLoginTrialsCount
+
+           IF wLoginTrialsCount > 1 THEN
+               DISPLAY "Combinaison non reconnue. Veuillez réessayer :"
+           END-IF
+     
+           ACCEPT fu_login
+           ACCEPT wPassword
+     
+          *> On cherche l'utilisateur pour voir s'il existe, si oui on 
+          *> regarde si le mot de passe correspond.
+           OPEN INPUT futil
+           READ futil
+           KEY IS fu_login
+               NOT INVALID KEY     MOVE fu_id TO wConnectedUser
+           END-READ
+           CLOSE futil
+
+           IF NOT wConnectedUser = 0 AND NOT wPassword = fu_mdp THEN
+              *> Mot de passe incorrect, on repasse à 0
+               MOVE 0 TO wConnectedUser
+           END-IF
+       END-PERFORM
+
+       IF wLoginTrialsCount = 3 AND wConnectedUser = 0 THEN
+           MOVE 1 TO wExitProgramme
+       END-IF
+       IF NOT wConnectedUser = 0 THEN
+           DISPLAY "Vous êtes connecté en tant que :"
+          *> PERFORM display_user_info
+       END-IF.
